56e7659b098c70bf6443bbf7a4b896bf4174853b
Quinn Hopwood <42815474+QuinnHop@users.noreply.github.com>
2019-12-08 18:02:41 -0500

    Got rid of address field in Results class

--- Web_apps_project_2/src/classes.js
+++ Web_apps_project_2/src/classes.js
@@ -1,7 +1,6 @@
 class result{
-    constructor(name = "", address = "", agency = "", route_id = 0, arrivalTime = 10000){
+    constructor(name = "", agency = "", route_id = 0, arrivalTime = 10000){
         this.name = name;
-        this.address = address;
         this.agency = agency;
         this.route_id = route_id;
         this.arrivalTime = arrivalTime;
