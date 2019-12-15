86ddeaf83811b3ce4f86e44bf252c175155b604c
Alex Pecore <33386022+altonapecore@users.noreply.github.com>
2019-12-15 18:26:30 -0500

    Added in success and danger cards

--- Web_apps_project_2/src/vue-component.js
+++ Web_apps_project_2/src/vue-component.js
@@ -1,17 +1,37 @@
 export default Vue.component('bus-result-success',{
 	props: ['result'],
 	template: 
-	`<div class="card text-white bg-success mb-3" style="width:90%;">
-		<div class="card-header">Arriving in: {{result.arrivalTime}}</div>
-		<div class="card-body">
-		<h5 class="card-title">{{result.name}}</h5>
-		<p class="card-text>This Stop services routes: </p>
-		<span class="card-text" v-for="route in result.route_id">
-    		<span>, </span>
-		</span>
-		<p class="card-text">This Bus stop is serviced by {{result.agency_id}}</p>
+	`
+	<b-card 
+	v-if="result.isRunning"
+	bg-variant="success"
+	text-variant="white"
+	class="col-lg-4"
+	>
+		<div style="margin-right:2em;" class="card-header">Arriving at: {{result.arrivalTime}}</div>
+		<div style="margin-right:2em;" class="card-body">
+			<h5 class="card-title">{{result.name}}</h5>
+			<p class="card-text>This Stop services routes: </p>
+			<span class="card-text" v-for="route in result.route_id">
+    			<span>, </span>
+			</span>
 		</div>
-  	</div>
+	</b-card>
+	<b-card 
+	v-else
+	bg-variant="danger"
+	text-variant="white"
+	class="col-lg-4"
+	>
+		<div style="margin-right:2em;" class="card-header">Arriving at: {{result.arrivalTime}}</div>
+		<div style="margin-right:2em;" class="card-body">
+			<h5 class="card-title">{{result.name}}</h5>
+			<p class="card-text>This Stop services routes: </p>
+			<span class="card-text" v-for="route in result.route_id">
+				<span>, </span>
+			</span>
+		</div>
+	</b-card>
 	`
 	
 	
